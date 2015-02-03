059de2e9a12c9a5131fdc9141bbf27e6f69694e6
Patrick Sobolewski <psobolew@LB266-27.adit.mines.edu>
2015-02-02 19:25:10 -0700

    'zz' is not recognized as an internal or external command, operable program or batch file.

--- src/gitTest/helloGit.java
+++ src/gitTest/helloGit.java
@@ -1,10 +1,13 @@
 package gitTest;
 
 public class helloGit {
-
+	
 	public static void main(String args[]) {
 		System.out.println("Hello");
 		System.out.println("Chap!");
 		System.out.println("How do you do?");
+		System.out.println("I have no feeling, I am but a tool, "
+				+ "a means to an end, one who cannot feel, cannot "
+				+ "reason...I am but a shell of a being");
 	}
 }
